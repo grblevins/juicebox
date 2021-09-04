fa886589c12abf71b458cc114e024f9a74df3130
Grady Blevins <grblevins2@gmail.com>
2021-09-04 12:15:38 -0500

    finished part 1

--- db/index.js
+++ db/index.js
@@ -1,7 +1,33 @@
 const { Client }  = require('pg');
 
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+  }
+
+  async function createUser({ username, password }) {
+    try {
+      const { rows } = await client.query(`
+        INSERT INTO users(username, password)
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING *;
+      `, [username, password]);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  }
+
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 module.exports = {
     client,
+    getAllUsers,
+    createUser,
 }
